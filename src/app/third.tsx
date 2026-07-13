fec6f797e3067665702d46f574ba53c9cf020ba3
nelvison-benedetto <119800037+nelvison-benedetto@users.noreply.github.com>
2026-07-13 17:48:25 +0200

    update

--- src/app/third.tsx
+++ src/app/third.tsx
@@ -1,50 +1,71 @@
+import { useHeaderHeight } from "@react-navigation/elements";
 import { useState } from "react";
 import {
     Alert,
     KeyboardAvoidingView,
-    Platform,
     Pressable,
+    ScrollView,
     Text,
     TextInput,
     View,
 } from "react-native";
 
 export default function Third() {
-  const [text, onChangeText] = useState("initial text");
   const [number, onChangeNumber] = useState("");
+  const headerHeight = useHeaderHeight(); //altezza reale dell'header dello Stack, sopra questo screen
 
   function XXX(message: string): void {
     Alert.alert(message);
   }
 
   return (
-    <>
-      <KeyboardAvoidingView
-        className="flex-1"
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        keyboardVerticalOffset={80}
+    <KeyboardAvoidingView
+      className="flex-1"
+      behavior="padding"
+      keyboardVerticalOffset={headerHeight}
+    >
+      <ScrollView
+        className="flex-1 mt-[1rem]"
+        contentContainerClassName="flex-grow p-4"
+        keyboardShouldPersistTaps="handled"
+        automaticallyAdjustKeyboardInsets={true}
       >
+        <Text>heheh</Text>
+
         <View>
-          <Text>heheh</Text>
-          <TextInput
-            value={number}
-            onChangeText={onChangeText}
-            keyboardType="numeric"
-            placeholder="insert a number"
-            className="border border-yellow-600"
-          />
-          <View>
-            <Pressable
-              onPress={() => {
-                XXX("number");
-              }}
-              className="bg-black"
-            >
-              <Text className="text-white">Btn</Text>
-            </Pressable>
-          </View>
+          <Pressable
+            onPress={() => {
+              XXX("number");
+            }}
+            className="bg-black py-[6rem] mb-2"
+          >
+            <Text className="text-white text-2xl">Btnbutonn nutton</Text>
+          </Pressable>
+          <Pressable
+            onPress={() => {
+              XXX("number");
+            }}
+            className="bg-black py-[6rem] mb-2"
+          >
+            <Text className="text-white text-2xl">Btnbutonn nutton</Text>
+          </Pressable>
+          <Pressable
+            onPress={() => {
+              XXX("number");
+            }}
+            className="bg-black py-[6rem]"
+          >
+            <Text className="text-white text-2xl">Btnbutonn nutton</Text>
+          </Pressable>
         </View>
-      </KeyboardAvoidingView>
-    </>
+      </ScrollView>
+      <TextInput
+        value={number}
+        onChangeText={onChangeNumber}
+        keyboardType="numeric"
+        placeholder="insert a number"
+        className="border border-yellow-600 "
+      />
+    </KeyboardAvoidingView>
   );
 }
