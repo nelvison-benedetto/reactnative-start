becdfe6b1cd8076e0c9a664ba74ad250f4a6e10b
nelvison-benedetto <119800037+nelvison-benedetto@users.noreply.github.com>
2026-07-13 08:31:12 +0200

    update

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,13 @@
-import { Alert, Button, Pressable, Text, View } from "react-native";
+import { useState } from "react";
+import {
+  Alert,
+  Button,
+  Image,
+  Pressable,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function Index() {
   function MyTest() {
@@ -13,14 +22,11 @@ export default function Index() {
     Alert.alert(message);
   }
 
+  const [text, onChangeText] = useState("default text");
+  const [number, onChangeNumber] = useState("");
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View className="justify-center items-center flex-1 flex-col">
       <Text className="text-xl font-bold text-blue-600">
         Welcome to Nativewind!
       </Text>
@@ -34,6 +40,22 @@ export default function Index() {
       >
         <Text className="text-white font-semibold">pressablewhattddt</Text>
       </Pressable>
+
+      <View className="w-1/2 h-60 rounded-full  overflow-hidden border-red-600 border-2">
+        <Image
+          source={require("../../assets/images/competesrl-working.jpg")}
+          className="w-full h-full"
+          resizeMode="cover"
+        />
+      </View>
+      <View className="h-30 w-full" />
+      <TextInput
+        className="border-2"
+        onChangeText={onChangeText}
+        value={number}
+        keyboardType="numeric"
+        placeholder="insert only nums"
+      />
     </View>
   );
 }
