dcbb6ff9efa129d06e22fff4936a239f88f1ebb1
nelvison-benedetto <119800037+nelvison-benedetto@users.noreply.github.com>
2026-07-13 17:58:27 +0200

    update

--- src/app/third.tsx
+++ src/app/third.tsx
@@ -2,21 +2,54 @@ import { useHeaderHeight } from "@react-navigation/elements";
 import { useState } from "react";
 import {
     Alert,
+    Button,
     KeyboardAvoidingView,
     Pressable,
     ScrollView,
+    StatusBar,
+    StatusBarStyle,
     Text,
     TextInput,
     View,
 } from "react-native";
 
+const STYLES = ["default", "dark-content", "light-content"] as const;
+const TRANSITIONS = ["fade", "slide", "none"] as const;
+
 export default function Third() {
   const [number, onChangeNumber] = useState("");
   const headerHeight = useHeaderHeight(); //altezza reale dell'header dello Stack, sopra questo screen
-
   function XXX(message: string): void {
     Alert.alert(message);
   }
+
+  //x status bar
+  const [hidden, setHidden] = useState(false);
+  const [statusBarStyle, setStatusBarStyle] = useState<StatusBarStyle>(
+    STYLES[0],
+  );
+  const [statusBarTransition, setStatusBarTransition] = useState<
+    "fade" | "slide" | "none"
+  >(TRANSITIONS[0]);
+  const changeStatusBarVisibility = () => setHidden(!hidden);
+
+  const changeStatusBarStyle = () => {
+    const styleId = STYLES.indexOf(statusBarStyle) + 1;
+    if (styleId === STYLES.length) {
+      setStatusBarStyle(STYLES[0]);
+    } else {
+      setStatusBarStyle(STYLES[styleId]);
+    }
+  };
+
+  const changeStatusBarTransition = () => {
+    const transition = TRANSITIONS.indexOf(statusBarTransition) + 1;
+    if (transition === TRANSITIONS.length) {
+      setStatusBarTransition(TRANSITIONS[0]);
+    } else {
+      setStatusBarTransition(TRANSITIONS[transition]);
+    }
+  };
 
   return (
     <KeyboardAvoidingView
@@ -57,6 +90,31 @@ export default function Third() {
           >
             <Text className="text-white text-2xl">Btnbutonn nutton</Text>
           </Pressable>
+        </View>
+        <StatusBar
+          animated={true}
+          backgroundColor="#61dafb"
+          barStyle={statusBarStyle}
+          showHideTransition={statusBarTransition}
+          hidden={hidden}
+        />
+        <Text>
+          StatusBar Visibility:{"\n"}
+          {hidden ? "Hidden" : "Visible"}
+        </Text>
+        <Text>
+          StatusBar Style:{"\n"}
+          {statusBarStyle}
+        </Text>
+        <View>
+          <Button
+            title="Toggle StatusBar"
+            onPress={changeStatusBarVisibility}
+          />
+          <Button
+            title="Change StatusBar Style"
+            onPress={changeStatusBarStyle}
+          />
         </View>
       </ScrollView>
       <TextInput
